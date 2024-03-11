af18d6a45aba623ddd695fe62d8d9780907bef16
cheikh tidiane <cheikh951812@gmail.com>
2024-03-11 15:28:11 +0100

    Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -10,39 +11,14 @@ import { ArticleComponent } from './article/article.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  listeArticle: any;
+  constructor(private dataservice: DataService) {}
+  ngOnInit(): void {
+    this.listeArticle = this.dataservice.listeArticle
+  }
   message: string = '';
 
-  listeArticle = [
-    {
-      id: 1,
-      titreArticle: 'Iphone 15',
-      prixArticle: 1100,
-      description: 'iPhone 15 128GB Noir',
-      urlImg: '../assets/images/iphone-15-pro.jpg',
-      altImg: 'Iphone 15',
-      dispo: true,
-    },
-    {
-      id: 2,
-      titreArticle: 'MacBook Air M3',
-      prixArticle: 2100,
-      description: 'Apple - 13,6" MacBook Air M3 (2024)',
-      urlImg: '../assets/images/Macbook.jpg',
-      altImg: 'MacBook Air M3',
-      dispo: false,
-    },
-    {
-      id: 3,
-      titreArticle: 'Apple watch serie 9',
-      prixArticle: 940,
-      description: 'Apple Watch Series 9 GPS - 41mm',
-      urlImg: '../assets/images/apple-w.jpg',
-      altImg: 'Apple watch serie 6',
-      dispo: true,
-    },
-  ];
+  
 
   onAffiche(arg: string): string {
     return (this.message = `Merci d'avoir Voté pour l'article: ${arg}`);
---
+++ src/app/data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  listeArticle = [
+    {
+      id: 1,
+      titreArticle: 'Iphone 15',
+      prixArticle: 1100,
+      description: 'iPhone 15 128GB Noir',
+      urlImg: '../assets/images/iphone-15-pro.jpg',
+      altImg: 'Iphone 15',
+      dispo: true,
+    },
+    {
+      id: 2,
+      titreArticle: 'MacBook Air M3',
+      prixArticle: 2100,
+      description: 'Apple - 13,6" MacBook Air M3 (2024)',
+      urlImg: '../assets/images/Macbook.jpg',
+      altImg: 'MacBook Air M3',
+      dispo: false,
+    },
+    {
+      id: 3,
+      titreArticle: 'Apple watch serie 9',
+      prixArticle: 940,
+      description: 'Apple Watch Series 9 GPS - 41mm',
+      urlImg: '../assets/images/apple-w.jpg',
+      altImg: 'Apple watch serie 6',
+      dispo: true,
+    },
+  ];
+  constructor() { }
+}
