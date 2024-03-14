f23ba491e37b1a35ddf346e135ea64fe4cd1ca48
cheikh tidiane <cheikh951812@gmail.com>
2024-03-14 15:13:20 +0100

    error 404: article

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,10 @@ export const routes: Routes = [
   {
     path: 'liste',
     redirectTo: '',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'Oops ! Page Not Found',
   },
 ];
