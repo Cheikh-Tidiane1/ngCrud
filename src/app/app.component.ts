7a6fb896fb2b90940b4e04da682e9207eea074a5
cheikh tidiane <cheikh951812@gmail.com>
2024-03-16 15:53:21 +0100

    Rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
-import { DataService } from './data.service';
-
+import { interval , Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,7 +10,24 @@ import { DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit ,OnDestroy {
+  secondes: string;
+  compteurSubscriber: Subscription ;
   constructor() {}
-  ngOnInit(): void {}
+  
+  ngOnInit(): void {
+    const compteur = interval(1000).pipe(
+      map(value => value % 2 == 0 ? `${value} est pair`: `${value} est impair`)
+    ) ;
+
+    this.compteurSubscriber = compteur.subscribe({
+      next: (v) => this.secondes = v,
+      error: (e) => console.log(e),
+      complete: () => console.log("complete")
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.compteurSubscriber.unsubscribe();
+  }
 }
